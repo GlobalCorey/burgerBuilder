3d5225c9fac5552bd49c68414e22b86d896aaf6f
Corey <corey4994@gmail.com>
2019-09-16 21:42:00 -0500

    Added css and initial burger builder functionality

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import classes from './burgerIngredient.module.css'
+import classes from './BurgerIngredient.module.css'
 
 const burgerIngredient = (props) =>{
     let ingredient = null;
@@ -33,6 +34,10 @@ const burgerIngredient = (props) =>{
             ingredient = null;
     }
     return ingredient;
+};
+
+burgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 };
 
 export default burgerIngredient;
