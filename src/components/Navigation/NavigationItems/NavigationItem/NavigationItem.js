0ae3b021b7cd0d3d350e323c2f390e9e9ac7b6c9
Corey <corey4994@gmail.com>
2019-10-25 13:32:59 -0500

    Basic Burger builder app done. Next is HTTP and advanced React

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
+    
     <li className={classes.NavigationItem}>
         <a 
             href={props.link} 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) =>(
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
+class Modal extends Component{
+    render(){
+        return(
+            <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
         <div 
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}>
-            {props.children}
+            {this.props.children}
         </div>
     </Aux>
-    
-)
-
-export default modal;
+        );
+    };
+}
+//memo prevents Order summary from re-rendering repeatedly with ingredients state
+export default React.memo(Modal);
